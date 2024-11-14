d4af73b03fc8b1b8ab9f94c3a6638872403509b3
USER <USER@DESKTOP-L057421>
2024-11-15 00:16:52 +0900

    myPageGeneralUser.html 실행 컨트롤러 추가

---
+++ campProject/src/main/java/com/example/campProject/controller/MyPageGeneralUserController.java
@@ -0,0 +1,13 @@
+package com.example.campProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyPageGeneralUserController {
+
+    @GetMapping("/myPageGeneralUser")
+    public String myPageGeneralUser() {
+        return "myPageGeneralUser"; // myPageGeneralUser.html 파일을 반환
+    }
+}
