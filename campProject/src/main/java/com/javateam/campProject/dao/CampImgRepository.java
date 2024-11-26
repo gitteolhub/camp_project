d33c46fc1b0c641ddfc041960c97abbb11ed54d2
gitteolhub <ht2001@naver.com>
2024-11-26 16:01:50 +0900

    캠핑장 데이터베이스 구축

---
+++ campProject/src/main/java/com/javateam/campProject/dao/CampImgRepository.java
@@ -0,0 +1,9 @@
+package com.javateam.campProject.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.javateam.campProject.domain.CampImgVO;
+
+public interface CampImgRepository extends CrudRepository<CampImgVO, Integer>{
+
+}
